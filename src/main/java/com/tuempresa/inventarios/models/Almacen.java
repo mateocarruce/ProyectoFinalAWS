b136a842fe403b2a84700cf2075b235840ac7c12
mateocarruce <mateocarruce@gmail.com>
2024-12-23 18:14:15 -0500

    New Proyect capa Inventario

---
+++ src/main/java/com/tuempresa/inventarios/models/Almacen.java
@@ -0,0 +1,22 @@
+package com.tuempresa.inventarios.models;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "Almacenes")
+@Data
+public class Almacen {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer almacenID;
+
+    @Column(nullable = false, length = 100)
+    private String nombre;
+
+    @Column(nullable = true)
+    private String ubicacion;
+
+    @Column(nullable = false)
+    private Integer capacidad;
+}
